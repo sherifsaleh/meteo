94cebbcfa94e8ba047d6e5088d61b47de1993719
Sherif SALEH <sherifsaleh111@gmail.com>
2016-07-13 23:02:52 +0200

    added weather api and linked with google maps

--- client/components/gmap/gmap.component.ts
+++ client/components/gmap/gmap.component.ts
@@ -17,6 +17,7 @@ import { WeatherService } from '../../service/weather.service';
 interface marker {
     lat: number;
     lng: number;
+    temp?: number;
     label?: string;
     draggable: boolean;
 }
@@ -31,10 +32,12 @@ interface marker {
 export class GmapComponent  {
     error: string;
     weather: any;
+    label : any; // Clicked marker city name
 
-    constructor ( private _http : Http ){}
-    //constructor(private _weatherService: WeatherService) { }
-
+    //constructor ( private _http : Http ){}
+    constructor(private _weatherService: WeatherService) { }
+    ngOnInit() {
+    }
 
     //AIzaSyCQBqWNTuOMLEao2-4BJy625u6TFKSAy_c
     // google maps zoom level
@@ -44,98 +47,109 @@ export class GmapComponent  {
     lat: number = 46.2276380;
     lng: number = 2.2137490;
 
+
     clickedMarker(label: string, index: number) {
-      console.log(`clicked the marker: ${label || index}`)
-    }
 
-    //  pushing marker on clicked
-    // mapClicked($event: MouseEvent) {
-    //   this.markers.push({
-    //     lat: $event.coords.lat,
-    //     lng: $event.coords.lng
-    //   });
-    // }
+       // setting the label city name to the search weather fun.
+       this._weatherService.label = label;
+       // calling the search weather fun.
+       this._weatherService.getWeather().subscribe(weather => {
 
-    markerDragEnd(m: marker, $event: MouseEvent) {
-      console.log('dragEnd', m, $event);
+           this.weather = weather;
+           // setting the temp for each marker
+           this.markers[index]['temp'] = weather.main.temp;
+       });
     }
 
     markers: marker[] = [
         {
             lat: 48.8566140,
             lng: 2.3522219,
+            temp: 1,
             label: 'Paris',
             draggable: false
         },
         {
             lat: 45.7640430,
             lng: 4.8356590,
+            temp: 1,
             label: 'Lyon',
             draggable: false
         },
         {
             lat: 43.2964820,
             lng:   5.3697800,
+            temp: 1,
             label: 'Marseille',
             draggable: false
         },
         {
             lat: 42.6972830,
             lng:   9.4508810,
+            temp: 1,
             label: 'Bastia',
             draggable: false
         },
         {
             lat: 41.9192290,
             lng:   8.7386350,
+            temp: 1,
             label: 'Ajaccio',
             draggable: false
         },
         {
             lat: 43.7384176,
             lng:   7.4246158,
+            temp: 1,
             label: 'Monaco',
             draggable: false
         },
         {
             lat: 43.6107690,
             lng:   3.8767160,
+            temp: 1,
             label: 'Montpellier',
             draggable: false
         },
         {
             lat: 43.6046520,
             lng:   1.4442090,
+            temp: 1,
             label: 'Tolouse',
             draggable: false
         },
         {
             lat: 44.8377890,
             lng:  -0.5791800,
+            temp: 1,
             label: 'Bordeaux',
             draggable: false
         },
         {
             lat: 47.2183710,
             lng:   -1.5536210,
+            temp: 1,
             label: 'Nantes',
             draggable: false
         },
         {
             lat: 48.3903940,
             lng:   -4.4860760,
+            temp: 1,
             label: 'Brest',
             draggable: false
         },
         {
             lat: 48.5734053,
             lng:   7.7521113,
+            temp: 1,
             label: 'Strasbourgs',
             draggable: false
         },
         {
             lat: 50.6292500,
             lng:   3.0572560,
+            temp: 1,
             label: 'Lille',
             draggable: false
         }
--- client/service/weather.service.ts
+++ client/service/weather.service.ts
@@ -10,11 +10,15 @@ export class WeatherService {
     private city='Paris,fr';
     private name= 'weather'; // api name
     private key= '43ef9b54ffc67bb76d09b030a41956e5';
+    label : any = 'paris'; // city name coming from the marker label
+
+    // array holding : label number, label city name, label city temp
+
     constructor(private _http: Http) {
     }
     getWeather(){
         // url by geoloc api.openweathermap.org/data/2.5/weather?lat=35&lon=139
-        return this._http.get('http://api.openweathermap.org/data/2.5/weather?q='+this.city+'&APPID='+this.key+'&units=metric')
+        return this._http.get('http://api.openweathermap.org/data/2.5/weather?q='+this.label+'&APPID='+this.key+'&units=metric')
                 .map(res => res.json());
     }
 }
